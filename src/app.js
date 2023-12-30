2b0cc70ff82134c2080c302c5f0fc3caa27d45e0
CaioMiranda12 <caioclm04@gmail.com>
2023-12-30 14:33:14 -0300

    cors added

--- src/app.js
+++ src/app.js
@@ -5,10 +5,15 @@ import cors from 'cors'
 
 import './database'
 
+const corsOptions = {
+  origin: 'https://re-codeburger-interface-3.vercel.app',
+  credentials: true,
+}
+
 class App {
   constructor() {
     this.app = express()
-    this.app.use(cors())
+    this.app.use(cors(corsOptions))
 
     this.middlewares()
     this.routes()
